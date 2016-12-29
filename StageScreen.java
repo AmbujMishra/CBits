6cbea263aa61cf9702fc65262867c4c60aceb7de
Ambuj <hiambuj4u@gmail.com>
2016-12-29 21:00:51 +0530

    Update StageScreen.java

--- StageScreen.java
+++ StageScreen.java
@@ -3,6 +3,7 @@ package com.kingam.cbits;
 *Ambuj Mishra
 * Created:	24 August 2016
 * Version:	1.0	(24 August 2016)
+* 28122016: Updated total score position for visibility
 */
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
@@ -110,10 +111,7 @@ public class StageScreen implements Screen{
 		game.CD.stringDrawing(game.sr,game.w*0.5f+pw+gap,2*y1/3+2*(h+3*gap+pw),w*0.5f,h*0.5f,pw*0.5f,gap,"BEST:"+game.pref.getFloat("2best"),game.CC.getCharColor());
 		
 		*/
-		//09 Oct 2016
-		game.CD.stringDrawing(game.sr,gap,game.h-3*h-gap,w*0.5f,h*0.5f,pw*0.5f,gap,"TOTAL SCORE:"+ tbt,game.CC.getCharColor());
-		//new stage screen on 14 09 2016
-				game.CD.stringDrawing(game.sr,game.w*0.5f-5*(w+gap),game.h-h-gap,w,h,pw,gap,"CATEGORIES",game.CC.getCharColor());
+
 				//game.CD.stringDrawing(game.sr,game.w*0.5f-5*(w+gap),game.h-4*h-gap,w,h,pw,gap,"CATEGORIES",game.CC.getCharColor());
 			//	game.CD.stringDrawing(game.sr,gap,y,w,h,pw,gap,"RATE",game.CC.getCharColor());
 			//	game.CD.stringDrawing(game.sr,x,y,w,h,pw,gap,"COLORS",game.CC.getCharColor());
@@ -216,6 +214,11 @@ public class StageScreen implements Screen{
 				game.CD.stringDrawing(game.sr,game.w*0.5f+pw+gap,2*y1/3+2*(3*gap+pw-h)-gap+h+2*gap+2*pw-h-gap-h,w*0.5f,h*0.5f,pw*0.5f,gap,"MAX SPEED:"+(int)game.pref.getFloat("2maxsp")/200+"X",game.CC.getCharColor());
 				game.CD.stringDrawing(game.sr,game.w*0.5f+pw+gap,2*y1/3+2*(3*gap+pw-h)-h-gap+h+2*gap+2*pw-h-gap-h,w*0.5f,h*0.5f,pw*0.5f,gap,"BEST:"+game.pref.getFloat("2best"),game.CC.getCharColor());
 				*/
+
+		//09 Oct 2016
+		game.CD.stringDrawing(game.sr,gap,game.h-4*h-gap,w*0.5f,h*0.5f,pw*0.5f,gap,"TOTAL SCORE:"+ tbt,game.CC.getCharColor());
+		//new stage screen on 14 09 2016
+		game.CD.stringDrawing(game.sr,game.w*0.5f-5*(w+gap),game.h-h-gap,w,h,pw,gap,"CATEGORIES",game.CC.getCharColor());
 				//25 09 2016 update: Adjusted height as per Mode screen
 				game.CD.stringDrawing(game.sr,gap+2*pw+2*w,2*gap+2*pw,w,h,pw,gap,"LEADERS",game.CC.getCharColor());
 				game.CD.stringDrawing(game.sr,game.w-6*(h+gap)-2*pw,2*gap+2*pw,w,h,pw,gap,"SUBMIT",game.CC.getCharColor());
@@ -223,17 +226,17 @@ public class StageScreen implements Screen{
 				game.CD.stringDrawing(game.sr,0,gap,game.w,2*pw,0,0,"_",game.CC.bitColor);		//hor 1
 				
 				game.CD.stringDrawing(game.sr,0,h+3*gap+2*pw,game.w,2*pw,0,0,"_",game.CC.bitColor); //ho2
-				
-				game.CD.stringDrawing(game.sr,0,game.h-3*gap-pw-3*h,game.w,2*pw,0,0,"_",game.CC.bitColor);  //top most hor
+
+				game.CD.stringDrawing(game.sr,0,game.h-3*gap-pw-4*h,game.w,2*pw,0,0,"_",game.CC.bitColor);  //top most hor
 				//game.CD.stringDrawing(game.sr,game.w*0.5f-pw,2*h+4*gap+2*pw,2*pw,game.h-2*h-5*gap-4*pw,0,0,"_",game.CC.bitColor);
-				game.CD.stringDrawing(game.sr,game.w*0.5f-pw,gap+2*pw,2*pw,game.h-3*h-3*gap-4*pw,0,0,"_",game.CC.bitColor); //middle ver
+				game.CD.stringDrawing(game.sr,game.w*0.5f-pw,gap+2*pw,2*pw,game.h-4*h-3*gap-4*pw,0,0,"_",game.CC.bitColor); //middle ver
 				//game.CD.stringDrawing(game.sr,game.w*0.5f-pw,h+2*gap+2*pw,2*pw,game.h-h-3*gap-4*pw,0,0,"_",game.CC.bitColor);
 				
-				game.CD.stringDrawing(game.sr,0,gap+2*pw,2*pw,game.h-3*h-3*gap-4*pw,0,0,"_",game.CC.bitColor);		//left ver
-				game.CD.stringDrawing(game.sr,game.w-2*pw,gap+2*pw,2*pw,game.h-3*h-3*gap-4*pw,0,0,"_",game.CC.bitColor); //right ver
+				game.CD.stringDrawing(game.sr,0,gap+2*pw,2*pw,game.h-4*h-3*gap-4*pw,0,0,"_",game.CC.bitColor);		//left ver
+				game.CD.stringDrawing(game.sr,game.w-2*pw,gap+2*pw,2*pw,game.h-4*h-3*gap-4*pw,0,0,"_",game.CC.bitColor); //right ver
 				
-				game.CD.stringDrawing(game.sr,gap,y1/3+2*(2*gap+pw)-h-gap,game.w-2*gap,2*pw,0,0,"_",game.CC.bitColor);//hor 3
-				game.CD.stringDrawing(game.sr,gap,2*y1/3+2*(2*gap+pw-h)-h-gap,game.w-2*gap,2*pw,0,0,"_",game.CC.bitColor);//hor4
+				game.CD.stringDrawing(game.sr,gap,y1/3+2*(2*gap+pw)-2*h-gap,game.w-2*gap,2*pw,0,0,"_",game.CC.bitColor);//hor 3
+				game.CD.stringDrawing(game.sr,gap,2*y1/3+2*(2*gap+pw-h)-2*h-gap,game.w-2*gap,2*pw,0,0,"_",game.CC.bitColor);//hor4
 				
 				game.CD.stringDrawing(game.sr,2*gap,y2-h-gap,w1,h1/6,6*pw,gap,"16",game.CC.getCharColor());
 				game.CD.stringDrawing(game.sr,x1-4*w+3*gap,y2-h-gap,w1,h1/6,6*pw,gap*1.5f,"32",game.CC.getCharColor());
@@ -244,23 +247,23 @@ public class StageScreen implements Screen{
 				game.CD.stringDrawing(game.sr,game.w*0.5f+pw+gap,2*(h+3*gap+pw)-gap,w*0.5f,h*0.5f,pw*0.5f,gap,"SPEED:"+(int)game.pref.getFloat("32maxsp")/200+"X",game.CC.getCharColor());
 				game.CD.stringDrawing(game.sr,game.w*0.5f+pw+gap,2*(h+3*gap+pw)-h-gap,w*0.5f,h*0.5f,pw*0.5f,gap,"BEST:"+game.pref.getFloat("32best"),game.CC.getCharColor());
 				
-				game.CD.stringDrawing(game.sr,gap+4*w,y2+h1/3-h-gap,w1,h1/6,6*pw,0,"4",game.CC.getCharColor());
-				game.CD.stringDrawing(game.sr,x1,y2+h1/3-h-gap,w1,h1/6,6*pw,0,"8",game.CC.getCharColor());	
+				game.CD.stringDrawing(game.sr,gap+4*w,y2+h1/3-2*h-gap,w1,h1/6,6*pw,0,"4",game.CC.getCharColor());
+				game.CD.stringDrawing(game.sr,x1,y2+h1/3-2*h-gap,w1,h1/6,6*pw,0,"8",game.CC.getCharColor());
 				
-				game.CD.stringDrawing(game.sr,2*gap+pw,y1/3+2*(3*gap+pw)-gap,w*0.5f,h*0.5f,pw*0.5f,gap,"SPEED:"+(int)game.pref.getFloat("4maxsp")/200+"X",game.CC.getCharColor());
-				game.CD.stringDrawing(game.sr,2*gap+pw,y1/3+2*(3*gap+pw)-h-gap,w*0.5f,h*0.5f,pw*0.5f,gap,"BEST:"+game.pref.getFloat("4best"),game.CC.getCharColor());
+				game.CD.stringDrawing(game.sr,2*gap+pw,y1/3+2*(3*gap+pw)-gap-h,w*0.5f,h*0.5f,pw*0.5f,gap,"SPEED:"+(int)game.pref.getFloat("4maxsp")/200+"X",game.CC.getCharColor());
+				game.CD.stringDrawing(game.sr,2*gap+pw,y1/3+2*(3*gap+pw)-2*h-gap,w*0.5f,h*0.5f,pw*0.5f,gap,"BEST:"+game.pref.getFloat("4best"),game.CC.getCharColor());
 				
-				game.CD.stringDrawing(game.sr,game.w*0.5f+pw+gap,y1/3+2*(3*gap+pw)-gap,w*0.5f,h*0.5f,pw*0.5f,gap,"SPEED:"+(int)game.pref.getFloat("8maxsp")/200+"X",game.CC.getCharColor());
-				game.CD.stringDrawing(game.sr,game.w*0.5f+pw+gap,y1/3+2*(3*gap+pw)-h-gap,w*0.5f,h*0.5f,pw*0.5f,gap,"BEST:"+game.pref.getFloat("8best"),game.CC.getCharColor());
+				game.CD.stringDrawing(game.sr,game.w*0.5f+pw+gap,y1/3+2*(3*gap+pw)-gap-h,w*0.5f,h*0.5f,pw*0.5f,gap,"SPEED:"+(int)game.pref.getFloat("8maxsp")/200+"X",game.CC.getCharColor());
+				game.CD.stringDrawing(game.sr,game.w*0.5f+pw+gap,y1/3+2*(3*gap+pw)-2*h-gap,w*0.5f,h*0.5f,pw*0.5f,gap,"BEST:"+game.pref.getFloat("8best"),game.CC.getCharColor());
 				
-				game.CD.stringDrawing(game.sr,gap+4*w,y2+2*h1/3-2*h-gap,w1,h1/6,6*pw,0,"1",game.CC.getCharColor());
-				game.CD.stringDrawing(game.sr,x1,y2+2*h1/3-2*h-gap,w1,h1/6,6*pw,0,"2",game.CC.getCharColor());
+				game.CD.stringDrawing(game.sr,gap+4*w,y2+2*h1/3-3*h-gap,w1,h1/6,6*pw,0,"1",game.CC.getCharColor());
+				game.CD.stringDrawing(game.sr,x1,y2+2*h1/3-3*h-gap,w1,h1/6,6*pw,0,"2",game.CC.getCharColor());
 				
-				game.CD.stringDrawing(game.sr,2*gap+pw,2*y1/3+2*(3*gap+pw-h)-gap,w*0.5f,h*0.5f,pw*0.5f,gap,"SPEED:"+(int)game.pref.getFloat("1maxsp")/200+"X",game.CC.getCharColor());
-				game.CD.stringDrawing(game.sr,2*gap+pw,2*y1/3+2*(3*gap+pw-h)-h-gap,w*0.5f,h*0.5f,pw*0.5f,gap,"BEST:"+game.pref.getFloat("1best"),game.CC.getCharColor());
+				game.CD.stringDrawing(game.sr,2*gap+pw,2*y1/3+2*(3*gap+pw-h)-gap-h,w*0.5f,h*0.5f,pw*0.5f,gap,"SPEED:"+(int)game.pref.getFloat("1maxsp")/200+"X",game.CC.getCharColor());
+				game.CD.stringDrawing(game.sr,2*gap+pw,2*y1/3+2*(3*gap+pw-h)-2*h-gap,w*0.5f,h*0.5f,pw*0.5f,gap,"BEST:"+game.pref.getFloat("1best"),game.CC.getCharColor());
 				
-				game.CD.stringDrawing(game.sr,game.w*0.5f+pw+gap,2*y1/3+2*(3*gap+pw-h)-gap,w*0.5f,h*0.5f,pw*0.5f,gap,"SPEED:"+(int)game.pref.getFloat("2maxsp")/200+"X",game.CC.getCharColor());
-				game.CD.stringDrawing(game.sr,game.w*0.5f+pw+gap,2*y1/3+2*(3*gap+pw-h)-h-gap,w*0.5f,h*0.5f,pw*0.5f,gap,"BEST:"+game.pref.getFloat("2best"),game.CC.getCharColor());
+				game.CD.stringDrawing(game.sr,game.w*0.5f+pw+gap,2*y1/3+2*(3*gap+pw-h)-gap-h,w*0.5f,h*0.5f,pw*0.5f,gap,"SPEED:"+(int)game.pref.getFloat("2maxsp")/200+"X",game.CC.getCharColor());
+				game.CD.stringDrawing(game.sr,game.w*0.5f+pw+gap,2*y1/3+2*(3*gap+pw-h)-2*h-gap,w*0.5f,h*0.5f,pw*0.5f,gap,"BEST:"+game.pref.getFloat("2best"),game.CC.getCharColor());
 				
 				//end
 		//game.BD.draw(game.sr,0,0,x1,y1, '0');
@@ -326,7 +329,7 @@ public class StageScreen implements Screen{
 		}
 		else if(getSub(Gdx.input.getX(), Gdx.input.getY()))
 		{
-			game.aoi.submitScore(987, 1);
+			game.aoi.submitScore(tbt, 1);
 			System.out.println("Submit All");
 		}
 		}
@@ -387,7 +390,7 @@ public class StageScreen implements Screen{
 	private boolean getStage(int x, int y) {		
         // To convert camera's y axis which starts from top corner to screens y axis which starts from bottom
 		y=game.h-y;	
-		if(y>h+3*gap+4*pw && y<y1/3+2*(2*gap+pw)-h-gap)
+		if(y>h+3*gap+4*pw && y<y1/3+2*(2*gap+pw)-2*h-gap)
 		{
 			if(x<game.w*0.5f)
 			{
@@ -402,7 +405,7 @@ public class StageScreen implements Screen{
 				return true;
 			}
 		}
-		else if (y>y1/3+2*(2*gap+2*pw)-h-gap && y<2*y1/3+2*(2*gap+pw-h)-h-gap)
+		else if (y>y1/3+2*(2*gap+2*pw)-2*h-gap && y<2*y1/3+2*(2*gap+pw-h)-2*h-gap)
 		{
 			if(x<game.w*0.5f)
 			{
@@ -417,7 +420,7 @@ public class StageScreen implements Screen{
 				return true;
 			}
 		}
-		else if (y>2*y1/3+2*(2*gap+2*pw-h)-h-gap && y<game.h-2*gap-pw-2*h-h-gap)
+		else if (y>2*y1/3+2*(2*gap+2*pw-h)-2*h-gap && y<game.h-2*gap-pw-3*h-h-gap)
 		{
 			if(x<game.w*0.5f)
 			{
